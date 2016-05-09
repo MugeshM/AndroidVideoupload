a4d7ab4be9fe3abe5b1804d9125d6a3ed9500105
Mugesh M <Mugesh M>
2016-05-09 11:18:43 +0530

    Initial commit

---
+++ app/src/main/java/com/project/mugeshm/videoupload/MainActivity.java
@@ -0,0 +1,186 @@
+package com.project.mugeshm.videoupload;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Path;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    SQLHelper sqlhelper;
+    private static final int REQUEST_TAKE_GALLERY_VIDEO = 0;
+    String filemanagerstring="";
+    String selectedpath="";
+    TextView tv;
+    Button bt,bt2;
+    private String filepath = null;
+    ImageView imageView;
+    FileInputStream fileInputStream = null;
+    ProgressDialog progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=(Button)findViewById(R.id.button);
+        bt2=(Button)findViewById(R.id.button2);
+        tv=(TextView)findViewById(R.id.textView);
+        imageView=(ImageView)findViewById(R.id.imageView);
+
+        sqlhelper=new SQLHelper(this);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("video/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Video"),REQUEST_TAKE_GALLERY_VIDEO);
+
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileInputStream fstrm;
+                try {
+                    fstrm = new FileInputStream(selectedpath);
+                    Send_Now(fstrm);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_TAKE_GALLERY_VIDEO) {
+                Uri selectedImageUri = data.getData();
+
+                filepath = getPath(selectedImageUri);
+                Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(filepath, MediaStore.Video.Thumbnails.MINI_KIND);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(getBaseContext(),"Bitmap",Toast.LENGTH_LONG).show();
+                // OI FILE Manager
+                filemanagerstring = selectedImageUri.getPath();
+
+                // MEDIA GALLERY
+                selectedpath = getPath(selectedImageUri);
+                if (selectedpath != null) {
+                    Toast.makeText(getBaseContext(), selectedpath,Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }
+    }
+
+    // UPDATED!
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+    void Send_Now(FileInputStream fStream){
+        fileInputStream = fStream;
+        /*progress = ProgressDialog.show(this, "Upload","wait, Uploading video to server", true);
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setIndeterminate(true);
+        */
+        progress = new ProgressDialog(MainActivity.this);
+        progress.setMessage("Uploading Video");
+        progress.setIndeterminate(false);
+        progress.setMax(100);
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.show();
+        uploadFile();
+    }
+
+    public void uploadFile() {
+
+        File myFile = new File(selectedpath);
+        //progress.setMax((int)(long)myFile.length());
+        RequestParams params = new RequestParams();
+        try {
+            params.put("uservideo", myFile);
+        } catch(FileNotFoundException e) {}
+
+// send request
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post("http://192.168.0.123:3000/api/video", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onProgress(long bytesWritten, long totalSize) {
+                super.onProgress(bytesWritten, totalSize);
+                Log.d("progress", "pos: " + bytesWritten + " len: " + totalSize);
+                double percent=((double)bytesWritten/(double)totalSize)*100;
+                Log.d("percent", "" +percent);
+               // tv.setText(String.valueOf(percent));
+                progress.setProgress((int) percent);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] bytes) {
+                progress.dismiss();
+                tv.setText("Successfully uploaded");
+                // Toast.makeText(getBaseContext(),"Successfully uploaded",Toast.LENGTH_SHORT).show();
+                Log.d("success", String.valueOf(statusCode));
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] bytes, Throwable throwable) {
+                // handle failure response
+                progress.dismiss();
+                tv.setText("Unable to upload try again");
+                // Toast.makeText(getBaseContext(),"Unable to upload try again",Toast.LENGTH_SHORT).show();
+                Log.d("Failure", String.valueOf(statusCode));
+
+            }
+
+        });
+
+    }
+}
+
+
---
+++ app/src/main/java/com/project/mugeshm/videoupload/SQLHelper.java
@@ -0,0 +1,43 @@
+package com.project.mugeshm.videoupload;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by MugeshM on 5/9/2016.
+ */
+
+
+public class SQLHelper extends SQLiteOpenHelper{
+
+    public static final String DATABASE_NAME = "uploaded.sqlite";
+    public static final String TABLE_NAME = "videos";
+
+
+    public SQLHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+     db.execSQL("create table " + TABLE_NAME + "(id integer primary key,filename text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertContact  (String filename)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("filename", filename);
+        db.insert("contacts", null, contentValues);
+        return true;
+    }
+
+}
